8e242366a554b8cbb871a9aa1dc871bdbc0dad0c
maximeferraille <maxime.ferraille@gmail.com>
2017-01-15 15:37:59 +0100

    modify single page & add responsive

--- wp-content/themes/maximeferraille/single-project.php
+++ wp-content/themes/maximeferraille/single-project.php
@@ -6,11 +6,27 @@
         <div class="project-title">
           <h1><?php echo get_post_field('project_title'); ?></h1>
           <div class="line"></div>
-        <?php echo get_post_field('project_description'); ?>
-        <a href="<?php echo get_post_field('project_link'); ?>"><?php echo get_post_field('project_link_name'); ?></a>
+          <?php echo get_post_field('project_description'); ?>
+          <a href="<?php echo get_post_field('project_link'); ?>"><?php echo get_post_field('project_link_name'); ?></a>
+        </div>
+
+        <div class="project-picture">
+
+          <?php
+          $images = rwmb_meta( 'project_picture' );
+
+          if ( !empty( $images ) ) {
+            foreach ( $images as $image ) {
+            echo "<img src='{$image['url']}' alt='{$image['title']}'><br />";
+            }
+          }
+           ?>
+
         </div>
        <?php endwhile; endif; ?>
       </div>
   </div>
 
 <?php get_footer(); ?>
+
+<img src="" alt="">
